27368e438ea9ccf209d46bc76721ed430da6a9a8
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2026-01-18 02:35:01 +0000

    mapped projects from js

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,43 +1,39 @@
+import Data from '../assets/projects'
+
 export default function Projects() {
 
-    function ProjectFormat () {
-        return(
-            <div className="project-card">
+          const ProjectFormat = Data.map((item) => {
+                      
+            const techBadges = item.tech.map((badge) => (
+            <span className="tech-badge">{badge}</span>
+        ))
+
+  return (
+    <div className="project-card">
                         <div className="project-header">
-                            <h3>Little Library</h3>
+                            <h3>{item.title}</h3>
                             <div className="project-links">
-                                <a href="https://github.com/DaniqueJ-R/milestone_4" rel='noopener' target="_blank" className="project-link">View Code</a>
-                                <a href="https://little-libraries-5596411363a4.herokuapp.com/" className="project-link">Live Demo</a>
+                                <a href={item.link.git} rel='noopener' target="_blank" className="project-link">View Code</a>
+                                <a href={item.link.live} className="project-link">Live Demo</a>
                             </div>
                         </div>
-                        <p className="project-description">
-                            A web app that helps readers track, review, and organize books into personalized shelves, making reading social and fun. 
-                            Features user authentication, CRUD operations for book management, custom shelving system, and secure payment processing via Stripe API.
-                        </p>
+                        <p className="project-description">{item.text}</p>
                         <div className="project-tech">
-                            <span className="tech-badge">Django</span>
-                            <span className="tech-badge">Python</span>
-                            <span className="tech-badge">JavaScript</span>
-                            <span className="tech-badge">PostgreSQL</span>
-                            <span className="tech-badge">Stripe API</span>
-                            <span className="tech-badge">Bootstrap</span>
+                             {techBadges}
                         </div>
-                    </div>
-        )
-    }
-    return(
+                    </div> 
+  )
+})
+
+    
+    return (
                 <section id="projects" className="projects">
             <div className="container">
                 <h2 className="section-title">Portfolio Projects</h2>
                 <div className="projects-grid">
 
+                    {ProjectFormat}
 
-                    <ProjectFormat />
-
-
-                                                 
-
-                    
                 </div>
             </div>
         </section>
